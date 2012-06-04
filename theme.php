24b890851270459113c7da69547bebc0ae3f3c4a
nickpyett <contact@nickpyett.com>
2012-06-04 23:36:37 +0100

    Works well in Chrome 19, needs JS/CSS polyfills/shims

---
+++ theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Normal extends Theme {
+
+    public $name = 'Normal';
+    public $author = 'Nick Pyett';
+    public $author_website = 'http://nickpyett.com';
+    public $website = 'http://nickpyett.com';
+    public $description = 'A blank theme using normalize.css';
+    public $version = '1.0';
+
+}
+
+/* End of file theme.php */
